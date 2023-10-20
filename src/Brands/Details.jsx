3214344dc8d2622377ace453d8077cdd07e83f05
SALAB\dellv <salabyaham@gmail.com>
2023-10-20 06:06:36 +0600

    add the product in cart item

--- src/Brands/Details.jsx
+++ src/Brands/Details.jsx
@@ -1,8 +1,34 @@
 import { Link, useLoaderData } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 
 const Details = () => {
     const productDetails = useLoaderData()
+    const handleAddtocart = ()=>{
+        const img = productDetails.img
+        const name = productDetails.name
+        const brandname = productDetails.brandname
+        const price = productDetails.price
+        const carts = {img,name,brandname,price}
+
+        fetch('http://localhost:5000/brands/carts',{
+            method:"POST",
+            headers: {
+                'content-type' : 'application/json' 
+            },
+            body : JSON.stringify(carts)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire('You have successfully add the product in carts item')
+
+                
+            }
+        })
+
+    }
     return (
         <div className="mt-4">
             <p className="text-4xl border-b-4 border-b-yellow-950 font-extrabold text-orange-700 text-center  p-2">Details </p>
@@ -13,7 +39,7 @@ const Details = () => {
                 <div className="card-body">
                     <h2 className="card-title text-center text-white mt-2 font-semibold justify-center">{productDetails.name}</h2>
                     <p className="text-center text-white mt-2 font-semibold justify-center"> {productDetails.description}</p>
-                    <Link><button className="btn text-orange-700 ml-12 ">Add To cart</button></Link>
+                    <Link><button onClick={handleAddtocart} className="btn text-orange-700 ml-12 ">Add To cart</button></Link>
                 </div>
                 
             </div>
